b84b2e79c6fa5cbea12a8a6dd88cbcf65708b686
andina87 <115124879+andina87@users.noreply.github.com>
2022-11-11 16:30:46 -0800

    created header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 
 import React from 'react'
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => { 
   return (
     <> 
-      <hi>Welcome to my shop!</hi>
+    <Header />
+    <hi>Welcome to my shop!</hi>
+    <Footer />
     </>
   );
 }
